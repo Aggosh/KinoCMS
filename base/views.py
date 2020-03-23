3fd04eca9891d81ca6e6074d08a21ecbac7a6230
IAmArr0w <bkmzilia@gmail.com>
2020-03-23 23:24:04 +0200

    Added home page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from movies.models import Movie
+import datetime
+import locale
 
 
 def index(request):
-    return render(request, 'index.html')
+    locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+    today = datetime.date.today()
+    context = {"today_movies": Movie.objects.filter(premiere_date__lte=today),
+               "after_movies": Movie.objects.filter(premiere_date__gt=today),
+               "today_date": today.strftime("%B %d").encode('windows-1251').decode('utf-8')}
+    return render(request, 'index.html', context=context)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=200,
+        blank=False,
+        verbose_name='Название фильма')
+    premiere_date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        verbose_name="Дата примьеры")
+    poster = models.ImageField(
+        upload_to='img/posters',
+        verbose_name="Постер")
+    slug = models.SlugField(
+        verbose_name='URL',
+        max_length=50,
+        unique=True,)
+
+    def __str__(self):
+        return(f'{self.title}')
